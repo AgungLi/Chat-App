836a0d08fef8a046085ddf9f7a2ece4a46fe0d14
Agung Laksmana Ismail <agungismail414@gmail.com>
2021-09-07 14:00:31 +0700

    Run Emulator

---
+++ lib/app/utils/error_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Terjadi Kesalahan"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:chatapp/app/utils/error_page.dart';
+import 'package:chatapp/app/utils/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,11 +8,31 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorPage();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            title: "ChatApp",
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+
+        return LoadingPage();
+      },
+    );
+  }
 }
