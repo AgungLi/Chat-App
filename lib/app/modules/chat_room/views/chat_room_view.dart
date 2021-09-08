7615a289e072dcecc904f0faaabc867efa33df4e
Agung Laksmana Ismail <agungismail414@gmail.com>
2021-09-08 17:11:16 +0700

    Create Chat Room Screen

--- lib/app/modules/chat_room/views/chat_room_view.dart
+++ lib/app/modules/chat_room/views/chat_room_view.dart
@@ -9,15 +9,155 @@ class ChatRoomView extends GetView<ChatRoomController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ChatRoomView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ChatRoomView is working',
-          style: TextStyle(fontSize: 20),
+        backgroundColor: Colors.red[900],
+        leadingWidth: 100,
+        leading: InkWell(
+          onTap: () => Get.back(),
+          borderRadius: BorderRadius.circular(100),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(width: 5),
+              Icon(Icons.arrow_back),
+              SizedBox(width: 5),
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.grey,
+                child: Image.asset("assets/logo/noimage.png"),
+              ),
+            ],
+          ),
         ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Lorem Ipsum',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'statusnya lorem',
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: false,
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: ListView(
+                children: [
+                  ItemChat(
+                    isSender: true,
+                  ),
+                  ItemChat(
+                    isSender: false,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              width: Get.width,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: 200,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.emoji_emotions_outlined),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.red[900],
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(100),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemChat extends StatelessWidget {
+  const ItemChat({
+    Key? key,
+    required this.isSender,
+  }) : super(key: key);
+
+  final bool isSender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: 20,
+      ),
+      color: Colors.black12,
+      child: Column(
+        crossAxisAlignment:
+            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red[900],
+              borderRadius: isSender
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    ),
+            ),
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Lorem ipsum jdaei ajdrgp fih",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 5),
+          Text("18:30 PM"),
+        ],
+      ),
+      alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
     );
   }
 }
