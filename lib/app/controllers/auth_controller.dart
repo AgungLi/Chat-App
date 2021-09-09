e09fbc63c45d17804e892e8f5bad61caea721f5e
Agung Laksmana Ismail <agungismail414@gmail.com>
2021-09-09 10:26:48 +0700

    Google Sign In

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,15 +1,38 @@
 import 'package:chatapp/app/routes/app_pages.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthController extends GetxController {
   var isSkipIntro = false.obs;
   var isAuth = false.obs;
 
-  void login() {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount? _currentUser;
+
+  Future<void> login() async {
+    // Create Function for login with google.
+    try {
+      await _googleSignIn.signOut();
+      await _googleSignIn.signIn().then((value) => _currentUser = value);
+      await _googleSignIn.isSignedIn().then((value) {
+        if (value) {
+          // login is success
+          print(_currentUser);
+          isAuth.value = true;
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          // login is failed
+
+        }
+      });
+    } catch (error) {
+      print(error);
+    }
     Get.offAllNamed(Routes.HOME);
   }
 
-  void logout() {
+  Future<void> logout() async {
+    await _googleSignIn.signOut();
     Get.offAllNamed(Routes.LOGIN);
   }
 }
